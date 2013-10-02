0bd9408348328a2777d1ad56a82645f8dc44e4b1
tigeral <jRflk8J5>
2013-10-03 02:55:19 +0300

    I had updated the BitmapFilters class to correct handling of image bitmap data and moved out lambda function to separate enum.

--- java8/bitmapfilters/src/BitmapFilters.java
+++ java8/bitmapfilters/src/BitmapFilters.java
@@ -3,7 +3,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.function.IntConsumer;
+import java.util.function.IntUnaryOperator;
 import java.util.stream.IntStream;
 
 /**
@@ -18,44 +18,65 @@ import java.util.stream.IntStream;
  * So, we will wrap the image data with a Stream object, then apply some lambdas to process this data and finally we
  * will collect modified data to the result int[].
  * Complete image data will be stored into result.png file.
- *
- * This program is just a copy of that one which we create during java.io 2.0 RnD Lab contest in Kharkiv on 28.09.2013.
  */
 public class BitmapFilters {
 
     public static void main(String[] args) throws IOException {
-
         // open image file (file was loaded from the www.stockfreeimages.com web site
         // (http://www.stockfreeimages.com/21049591/Color-Creative-background-07.html)
         File imgPath = new File("./image1.png");
         BufferedImage bufferedImage = ImageIO.read(imgPath);
-        // read image data to int[] data buffer
-        int[] imgData = new int[bufferedImage.getWidth() * bufferedImage.getHeight()];
-        for (int y = 0; y < bufferedImage.getHeight(); y++ ) {
-            for (int x = 0; x < bufferedImage.getWidth(); x++ ) {
-                int pixelIndex = y * bufferedImage.getWidth() + x;
-                imgData[pixelIndex] = bufferedImage.getRGB(x, y);
-            }
-        }
+        int[] imgData = bufferedImage.getRGB(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(), null, 0, bufferedImage.getWidth());
 
-        // define result data int[] object which would be used from the lambda function.
-        int[] resultData = new int[bufferedImage.getWidth() * bufferedImage.getHeight()];
-        // define the index to fill resultData object in a strict order. We put index variable into array
-        // in order to allow its modification from lambda function,because lambdas couldn't use a non final fields.
-        int[] index = {0};
-
-        // define the lambda functions. (uncomment that one which you want to use)
-        IntConsumer filter = (pixelData) -> { resultData[index[0]++] = pixelData & 0xff0000;}; // only red chanel
-//        IntConsumer filter = (pixelData) -> { resultData[index[0]++] = pixelData & 0x00ff00;}; // only red chanel
-//        IntConsumer filter = (pixelData) -> { resultData[index[0]++] = pixelData & 0x0000ff;}; // only red chanel
-//        IntConsumer filter = (pixelData) -> { resultData[index[0]++] = pixelData & 0x00ffff;}; // red color excluded
-
+        // define the lambda function to apply some visual effects to image
+        IntUnaryOperator resultFunction = Filters.EXCEPT_RED.andThen(Filters.SEPIA.andThen(Filters.TO_GRAYSCALE));
         // wrap image data with a Stream and apply lambda function to it,
         IntStream imgDataStream = Arrays.stream(imgData);
-        imgDataStream.forEach(filter);
-
+        int[] resultData = imgDataStream.map(resultFunction).toArray();
         // save resultData to file
         bufferedImage.setRGB(0, 0, bufferedImage.getWidth(), bufferedImage.getHeight(), resultData, 0, bufferedImage.getWidth());
         ImageIO.write(bufferedImage, "png", new File("result.png"));
+    }
+
+    /**
+     * Define different visual effects as an enum in a lambda function notation.
+     * So this class both implements enum and IntUnaryOperator interface.
+     */
+    private enum Filters implements IntUnaryOperator {
+        ONLY_RED((pixelData) -> {return  pixelData & 0xff0000;}),
+        ONLY_GREEN((pixelData) -> {return  pixelData & 0x00ff00;}),
+        ONLY_BLUE((pixelData) -> {return  pixelData & 0x0000ff;}),
+        EXCEPT_RED((pixelData) -> {return  pixelData & 0x00ffff;}),
+        EXCEPT_GREEN((pixelData) -> {return  pixelData & 0xff00ff;}),
+        EXCEPT_BLUE((pixelData) -> {return  pixelData & 0xffff00;}),
+        TO_GRAYSCALE((pixelData) -> {
+            int red = (pixelData & 0xff0000) >> 16;
+            int green = (pixelData & 0x00ff00) >> 8;
+            int blue = pixelData & 0x0000ff;
+            int average = (red + green + blue) / 3;
+            return (average << 16) + (average << 8) + average;
+        }),
+        SEPIA((pixelData) -> {
+            int red = (pixelData & 0xff0000) >> 16;
+            int green = (pixelData & 0x00ff00) >> 8;
+            int blue = pixelData & 0x0000ff;
+            int resultRed = (int)Math.min((red * .393) + (green *.769) + (blue * .189), 255);
+            int resultGreen = (int)Math.min((red * .349) + (green *.686) + (blue * .168), 255);
+            int resultBlue = (int)Math.min((red * .272) + (green *.534) + (blue * .131), 255);
+            return (resultRed << 16) + (resultGreen << 8) + resultBlue;
+        });
+
+        private final IntUnaryOperator lambdaFunction;
+
+        // enum classes requires constructor to initialise an inner value (in our case it's a lambdaFunction field).
+        private Filters(IntUnaryOperator lambda) {
+            this.lambdaFunction = lambda;
+        }
+
+        // implement functional method of IntUnaryOperator interface.
+        @Override
+        public int applyAsInt(int operand) {
+            return lambdaFunction.applyAsInt(operand);
+        }
     }
 }
